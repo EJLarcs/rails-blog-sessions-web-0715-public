b392503252bc2d7d5037a5c8a9547a324e939c3a
EJLarcs <ejlarcombe@gmail.com>
2015-09-07 16:52:24 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 RailsBlog::Application.routes.draw do
+  root to: 'posts#index'
+
   resources :users
   resources :tags
-  
-  resources :posts do 
+  resources :sessions
+
+  resources :posts do
     resources :comments
   end
+
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+  get 'login', to: 'sessions#new'
+  # post '/sessions', to: 'sessions#create'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+  post 'logout', to: 'sessions#destroy'
 
 end
